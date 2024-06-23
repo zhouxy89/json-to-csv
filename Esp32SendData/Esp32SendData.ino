05398e3af19d5f0baa4e64ce0ac0d22377d0bb98
momahdi <mohamed4mahdi@gmail.com>
2024-06-23 09:52:23 +0200

    breakData

--- Esp32SendData/Esp32SendData.ino
+++ Esp32SendData/Esp32SendData.ino
@@ -7,34 +7,32 @@ const uint16_t port = 12345;  // The port on which your server is listening
 
 void setup() {
   Serial.begin(115200);
-  // WiFi.begin(ssid, password);
+  WiFi.begin(ssid, password);
 
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   delay(500);
-  //   Serial.println("Connecting to WiFi...");
-  // }
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi...");
+  }
 
-  // Serial.println("Connected to the WiFi network");
-  // Serial.print("Local IP address: ");
-  // Serial.println(WiFi.localIP());
+  Serial.println("Connected to the WiFi network");
+  Serial.print("Local IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-   int sensorValue = analogRead(A0); // Read the analog value from pin A0.
-  Serial.println(sensorValue); // Print the value to the Serial Monitor.
-  delay(200); // Wait 200 milliseconds before reading again.
-  // WiFiClient client;
+  WiFiClient client;
 
-  // if (!client.connect(host, port)) {
-  //   Serial.println("Connection to host failed");
-  //   delay(5000);  // Wait 5 seconds before retrying
-  //   return;
-  // }
+  if (!client.connect(host, port)) {
+    Serial.println("Connection to host failed");
+    delay(5000);  // Wait 5 seconds before retrying
+    return;
+  }
 
-  // String brakeData = "1037"; // Replace this with your actual brake data
-  // String pedalDataR = "1038";
-  // String pedalDataL = "1036";
-  // client.print(brakeData + "-" + pedalDataR + "-" + pedalDataL);
-  // Serial.println(brakeData + "-" + pedalDataR + "-" + pedalDataL);
-  // client.stop();
+  int sensorValue = analogRead(A1); // Read the analog value from pin A0.
+
+
+  String brakeData = "1038"; // sensorValue
+  client.print("brakeData=" + brakeData);
+  Serial.println("brakeData=" + brakeData);
+  client.stop();
 }
